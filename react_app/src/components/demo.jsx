c163e377426f6cf716977aaa93e7a67ae4da6db7
J <Jiajian.ou@gmail.com>
2019-07-15 19:19:36 -0400

    starting from scratch.

---
+++ react_app/src/components/demo.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Demo extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello</h1>
+                <h2>World</h2>
+            </div>
+         );
+    }
+}
+ 
+export default Demo;
